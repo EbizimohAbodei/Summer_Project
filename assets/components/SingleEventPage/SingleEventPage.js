ee72aab0ef69174faa9b7dc1e595092f15f8a8ec
ronttizz <toni.nissinen@gmail.com>
2022-05-24 14:03:17 +0300

    Edited CSS import

--- assets/components/SingleEventPage/SingleEventPage.js
+++ assets/components/SingleEventPage/SingleEventPage.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import classes from "./SingleEventPage.module.css";
+import "./SingleEventPage.module.css";
 import axios from "axios";
 
 const SingleEventPage = () => {
@@ -9,18 +9,18 @@ const SingleEventPage = () => {
   const [event, setEvent] = useState([]);
 
   return (
-    <div className={classes.eventPage}>
+    <div className="eventPage">
       <h3>
         {event.name}, {event.date}
       </h3>
-      <div className={classes.eventInfo}>
-        <div className={classes.leftColumn}>
+      <div className="eventInfo">
+        <div className="leftColumn">
           <p>{event.name}</p>
           <p>{event.date}</p>
           <p>{event.location}</p>
           <p>{event.website}</p>
         </div>
-        <div className={classes.rightColumn}>
+        <div className="rightColumn">
           <p>{event.description}</p>
         </div>
       </div>
