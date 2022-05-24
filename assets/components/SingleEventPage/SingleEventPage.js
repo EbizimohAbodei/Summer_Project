3b48e9453828cc506fc32a36d41da9d6dc549e7f
ronttizz <toni.nissinen@gmail.com>
2022-05-24 14:11:02 +0300

    File name change on import

--- assets/components/SingleEventPage/SingleEventPage.js
+++ assets/components/SingleEventPage/SingleEventPage.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "./SingleEventPage.module.css";
+import "./SingleEventPage.css";
 import axios from "axios";
 
 const SingleEventPage = () => {
