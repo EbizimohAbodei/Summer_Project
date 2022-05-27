2696eb096e1030148ca86ea6c9cf33ac5c5a2b32
ronttizz <toni.nissinen@gmail.com>
2022-05-27 13:58:28 +0300

    Basic information of the event displayed

--- assets/components/SingleEventPage/SingleEventPage.js
+++ assets/components/SingleEventPage/SingleEventPage.js
@@ -8,29 +8,38 @@ const SingleEventPage = () => {
   const params = useParams();
 
   const [event, setEvent] = useState([]);
+  const [place, setPlace] = useState([]);
 
   useEffect(() => {
     axios
-      .get("api.hel.fi/linkedevents/v1/event/" + params.id)
-      .then((res) => console.log(res.data))
+      .get("http://api.hel.fi/linkedevents/v1/event/" + params.id)
+      .then((res) => {
+        setEvent(res.data);
+        console.log(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
   return event ? (
     <div className="eventPage">
-      <h3>
-        {event.name}
-        {event.date}
-      </h3>
+      <h2>{event?.name?.fi}</h2>
       <div className="eventInfo">
         <div className="leftColumn">
-          <p>{event.name}</p>
-          <p>{event.date}</p>
-          <p>{event.location}</p>
-          <p>{event.website}</p>
+          <h4>{event?.name?.fi}</h4>
+          <p>
+            {new Date(event?.start_time).toLocaleDateString()},{" "}
+            {new Date(event?.start_time).toLocaleTimeString()} -{" "}
+            {new Date(event?.end_time).toLocaleDateString()},{" "}
+            {new Date(event?.end_time).toLocaleTimeString()}
+          </p>
+          <p>
+            {event?.short_description?.en ||
+              event?.short_description?.fi ||
+              event?.short_description?.sv}
+          </p>
         </div>
         <div className="rightColumn">
-          <p>{event.description}</p>
+          {event?.description?.en || event?.description?.fi || event?.description?.sv}
         </div>
       </div>
     </div>
