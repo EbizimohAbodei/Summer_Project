618c923b93c5fab6439e1821c762f7c738567413
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-05-29 19:15:28 +0300

    categories search, input search, prev page next page implemented

--- assets/components/SearchResultPage/SearchResultPage.js
+++ assets/components/SearchResultPage/SearchResultPage.js
@@ -5,8 +5,9 @@ import { Categories } from "../Categories/Categories";
 import "./SearchResultPage.scss";
 
 const SearchResultPage = () => {
-  const [events, setEvents] = useState([]);
   const searchResult = useLocation().state.response;
+  const [events, setEvents] = useState(searchResult.data);
+  const [meta, setMeta] = useState(searchResult.meta);
 
   const removeTags = (str) => {
     if (!str) {
@@ -17,10 +18,20 @@ const SearchResultPage = () => {
     return str.replace(/(<([^>]+)>)/gi, "");
   };
 
-  useEffect(() => {
-    setEvents(searchResult.data);
-  }, []);
+  const changePage = (fetch) => {
+    console.log("change page triggered");
+    axios.get(fetch).then((res) => {
+      console.log(res.data.meta);
+      setEvents(res.data.data);
+      setMeta(res.data.meta);
+    });
+  };
 
+  // useEffect(() => {
+  //   setEvents(searchResult.data);
+  //   setMeta(searchResult.meta);
+  // }, []);
+  console.log("meta: ", meta);
   if (!events) {
     return <p> loading ... </p>;
   }
@@ -34,25 +45,47 @@ const SearchResultPage = () => {
         } catch {
           image = "http://source.unsplash.com/afW1hht0NSs";
         }
-        const description =
-          event.description.en || event.description.fi || "no description";
+        const description = event.description
+          ? event.description.fi
+          : "no description";
+        let eventName = event.name
+          ? event.name.en || event.name.fi || event.name.sv
+          : "No name";
+        if (eventName.split(" ").length > 10) {
+          eventName = eventName.split(" ").slice(0, 10).join(" ") + "...";
+        }
         return (
           <div key={i} className="singleEvent">
             <img style={{ height: "10rem" }} src={image} />
             <div>
               <Link to={`/cards/${event?.id}`}>
-                <h1>{event.name.en || event.name.fi || event.name.sv}</h1>
+                <h1>{eventName}</h1>
               </Link>
               <em>
                 {event.street_address
                   ? `${event.street_address.fi},${event.address_locality.fi}`
                   : "no address"}
               </em>
-              <p>{removeTags(description).slice(0, 200)}...</p>
+              <p>{removeTags(description).slice(0, 200)}</p>
             </div>
           </div>
         );
       })}
+      <div>
+        {meta.previous && (
+          <button
+            className="prevButton"
+            onClick={() => changePage(meta.previous)}
+          >
+            Prev-page
+          </button>
+        )}
+        {meta.next && (
+          <button className="prevButton" onClick={() => changePage(meta.next)}>
+            Next-page
+          </button>
+        )}
+      </div>
     </div>
   );
 };
