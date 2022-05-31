fb2c3e0d986f14cdb4ba74561d790fecdf9cadb1
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-05-31 14:04:43 +0300

    added categories

--- assets/components/Categories/Categories.js
+++ assets/components/Categories/Categories.js
@@ -1,37 +1,10 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./categories.scss";
 
-export const Categories = () => {
+const CategoryList = ({ category }) => {
   const navigate = useNavigate();
-
-  const categories = [
-    { id: "yso:p4354", eventName: "Children" },
-    { id: "yso:p13050", eventName: "Families" },
-    { id: "yso:p6165", eventName: "Migrants" },
-    { id: "yso:p5590", eventName: "Adults" },
-    { id: "yso:p16486", eventName: "Students" },
-    { id: "yso:p2433", eventName: "Elderly" },
-    { id: "yso:p1235", eventName: "Films" },
-    { id: "yso:p1947", eventName: "Well-beign" },
-    { id: "yso:p14004", eventName: "Conversation" },
-    { id: "yso:p11185", eventName: "Conserts" },
-    { id: "yso:p360", eventName: "Culture" },
-    { id: "yso:p2739", eventName: "Fine Arts" },
-    { id: "yso:p916", eventName: "Exercise" },
-    { id: "yso:p15875", eventName: "Lectures" },
-    { id: "yso:p1808", eventName: "Music" },
-    { id: "yso:p5121", eventName: "Exhibitions" },
-    { id: "yso:p6062", eventName: "Games" },
-    { id: "yso:p3670", eventName: "Food" },
-    { id: "yso:p1278", eventName: "Dance" },
-    { id: "yso:p2625", eventName: "Theatre" },
-    { id: "yso:p19245", eventName: "Workshops" },
-    { id: "yso:p2771", eventName: "Outdoor" },
-    { id: "yso:p965", eventName: "Sports" },
-    { id: "yso:p26626", eventName: "Remote" },
-  ];
 
   const handleClick = (e) => {
     console.log(e.target.dataset.id);
@@ -49,19 +22,77 @@ export const Categories = () => {
   };
 
   return (
-    <div className="categories">
-      {categories.map((category, i) => {
-        return (
-          <span
-            data-id={category.id}
-            key={i}
-            onClick={(e) => handleClick(e)}
-            className="category"
-          >
-            {category.eventName}
-          </span>
-        );
-      })}
+    <div className="categoryItems">
+      <ul>
+        {category.map((item, i) => {
+          return (
+            <li data-id={item.id} onClick={(e) => handleClick(e)} key={i}>
+              {item.eventName}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
+
+export const Categories = ({ showCategory, showCategories }) => {
+  const categories = [
+    { id: "yso:p4354", eventName: "Children", category: "groupSegment" },
+    { id: "yso:p13050", eventName: "Families", category: "groupSegment" },
+    { id: "yso:p6165", eventName: "Migrants", category: "groupSegment" },
+    { id: "yso:p5590", eventName: "Adults", category: "groupSegment" },
+    { id: "yso:p2433", eventName: "Elderly", category: "groupSegment" },
+    { id: "yso:p2625", eventName: "Theatre", category: "arts" },
+    { id: "yso:p2739", eventName: "Fine Arts", category: "arts" },
+    { id: "yso:p1235", eventName: "Films", category: "arts" },
+    { id: "yso:p11185", eventName: "Conserts", category: "arts" },
+    { id: "yso:p1808", eventName: "Music", category: "arts" },
+    { id: "yso:p14004", eventName: "Conversation", category: "seeAndDo" },
+    { id: "yso:p360", eventName: "Culture", category: "seeAndDo" },
+    { id: "yso:p5121", eventName: "Exhibitions", category: "seeAndDo" },
+    { id: "yso:p3670", eventName: "Food", category: "seeAndDo" },
+    { id: "yso:p1278", eventName: "Dance", category: "seeAndDo" },
+    { id: "yso:p1947", eventName: "Well-beign", category: "activities" },
+    { id: "yso:p916", eventName: "Exercise", category: "activities" },
+    { id: "yso:p6062", eventName: "Games", category: "activies" },
+    { id: "yso:p2771", eventName: "Outdoor", category: "activities" },
+    { id: "yso:p965", eventName: "Sports", category: "activities" },
+    { id: "yso:p16486", eventName: "Students", category: "workAndStudy" },
+    { id: "yso:p15875", eventName: "Lectures", category: "workAndStudy" },
+    { id: "yso:p19245", eventName: "Workshops", category: "workAndStudy" },
+    { id: "yso:p26626", eventName: "Remote", category: "workAndStudy" },
+  ];
+
+  return (
+    <nav className="categories">
+      <ul>
+        <li onClick={() => showCategory("activities")}>Activities</li>
+        <li onClick={() => showCategory("arts")}>Arts</li>
+        <li onClick={() => showCategory("groupSegment")}>Groups</li>
+        <li onClick={() => showCategory("seeAndDo")}>See and Do</li>
+        <li onClick={() => showCategory("workAndStudy")}>Work and Study</li>
+        {showCategories.show && (
+          <CategoryList
+            category={categories.filter(
+              (category) => category.category === showCategories.category
+            )}
+          />
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+// {categories.map((category, i) => {
+//   return (
+//     <span
+//       key={i}
+// data-id={category.id}
+// onClick={(e) => handleClick(e)}
+//       className="category"
+//     >
+//       {category.eventName}
+//     </span>
+//   );
+// })}
