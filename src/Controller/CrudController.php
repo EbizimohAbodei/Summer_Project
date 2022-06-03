d6346a0a79c691423b62663f8963c218f8adcd73
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-06-03 13:56:15 +0300

    modified database

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\EventLikes;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,15 +15,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class CrudController extends AbstractController
 {
     #[Route('/addlikes', name: 'add_likes', methods: ["POST"] )]
-    public function newBlog(Request $request, ManagerRegistry $doctrine): Response
+    public function addLike(Request $request, ManagerRegistry $doctrine): Response
     {   
-        
-        
+        $date = new \Datetime($request->request->get("endDate"));
+    
         $em = $doctrine->getManager();
         $singleEvent = new EventLikes();
         $singleEvent->setEventId($request->request->get("eventId"));
         $singleEvent->setLikeCount($request->request->get("likeCount"));
         $singleEvent->setInterestCount($request->request->get("interestCount"));
+        $singleEvent->setEndDate($date);
 
         $em->persist($singleEvent);
         $em->flush();
@@ -35,4 +37,24 @@ class CrudController extends AbstractController
 
         return $this->json("Created new blog with an id of: " . $singleEvent->getId());
     } 
+
+
+    #[Route('/getlikes', name: 'get_likes', methods: ["GET"] )]
+    public function getLikes(EntityManagerInterface $em): Response
+    {
+        $likes = $em->getRepository(EventLikes::class)->findAll();
+        $data = [];
+
+        foreach ($likes as $like) {
+            $data[] = [
+                'id' => $like->getId(),
+                'eventId' => $like->getEventId(),
+                'likeCount' => $like->getLikeCount(),
+                'interestCount' => $like->getInterestCount(),
+                'endDate' => $like->getEndDate(),
+            ];
+        }
+
+        return $this->json($data);
+    }
 }
