a6abf3a75fb7ee6e7f2854fb8d7a3a221aee6cd9
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-06-03 11:03:31 +0300

    moved likebutton to cardslist

--- assets/components/CardsList/CardsList.jsx
+++ assets/components/CardsList/CardsList.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Card from "../Card/Card";
 import "./cardslist.scss";
+import { BsHeartFill } from "react-icons/bs";
+
 const axios = require("axios").default;
 import HeroBanner from "../HeroBanner/HeroBanner";
 
 function CardsList() {
   const [allEventsData, setAllEventsData] = useState([]);
   const [meta, setMeta] = useState([]);
+  const [postData, setPostData] = useState({
+    likeCount: 0,
+    interestCount: 0,
+  });
 
   useEffect(() => {
     axios
-      .get("http://api.hel.fi/linkedevents/v1/event/?end=2025-12-31&page=35&start=today")
+      .get(
+        "http://api.hel.fi/linkedevents/v1/event/?end=2025-12-31&page=35&start=today"
+      )
       .then((response) => {
         setAllEventsData(response?.data);
         setMeta(response.data.meta);
@@ -46,6 +54,21 @@ function CardsList() {
     window.scrollTo(0, 300);
   };
 
+  const handleLike = (e) => {
+    e.preventDefault();
+    console.log(e.target.dataset.id);
+    const { likeCount, interestCount } = postData;
+    let postForm = new FormData();
+    postForm.append("likeCount", likeCount + 1);
+    postForm.append("interestCount", interestCount + 1);
+    postForm.append("eventId", e.target.dataset.id);
+    axios
+      .post("http://127.0.0.1:8000/spa/addlikes", postForm)
+      .then((res) => console.log("form posted", res))
+      .catch((err) => console.log("error occurred: ", err));
+    setTest("");
+  };
+
   return (
     <div className="cardsListContainer">
       {console.log(allEventsData)}
@@ -58,7 +81,9 @@ function CardsList() {
             <Card
               key={item.id}
               id={item.id}
-              name={item.name.en || item.name.fi || item.name.sv || item.name.ru}
+              name={
+                item.name.en || item.name.fi || item.name.sv || item.name.ru
+              }
               locationCall={item.location["@id"]}
               startDate={new Date(item?.start_time).toLocaleDateString()}
               startTime={new Date(item?.start_time).toLocaleTimeString()}
@@ -81,7 +106,12 @@ function CardsList() {
                 item.short_description?.ru
               }
               eventImage={item?.images[0]?.url}
-            />
+            >
+              {" "}
+              <div onClick={(e) => handleLike(e)}>
+                <BsHeartFill data-id={item.id} />
+              </div>
+            </Card>
           );
         })}
       </div>
--- assets/components/SearchResultPage/SearchResultPage.js
+++ assets/components/SearchResultPage/SearchResultPage.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { BsHeartFill } from "react-icons/bs";
 import { useLocation, useNavigate } from "react-router-dom";
 import Card from "../Card/Card";
 import "./SearchResultPage.scss";
@@ -11,15 +10,14 @@ const SearchResultPage = () => {
   const [meta, setMeta] = useState(searchResult.meta);
   const [tags, setTags] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [postData, setPostData] = useState({
-    likeCount: 0,
-    interestCount: 0,
-  });
   const navigate = useNavigate();
 
   const getTags = (array) => {
     const getTags = array.map((tag, i) => {
       return tag.keywords.map((singleEvent) => {
+        if (!singleEvent["@id"]) {
+          return;
+        }
         return axios.get(singleEvent["@id"]);
       });
     });
@@ -79,21 +77,6 @@ const SearchResultPage = () => {
     );
   }
 
-  const handleLike = (e) => {
-    e.preventDefault();
-    console.log(e.target.dataset.id);
-    const { likeCount, interestCount } = postData;
-    let postForm = new FormData();
-    postForm.append("likeCount", likeCount + 1);
-    postForm.append("interestCount", interestCount + 1);
-    postForm.append("eventId", e.target.dataset.id);
-    axios
-      .post("http://127.0.0.1:8000/spa/addlikes", postForm)
-      .then((res) => console.log("form posted", res))
-      .catch((err) => console.log("error occurred: ", err));
-    setTest("");
-  };
-
   return (
     <div className="eventContainer">
       {events.map((event, i) => {
@@ -114,7 +97,6 @@ const SearchResultPage = () => {
             </li>
           );
         });
-        console.log(event.id);
         let image;
         try {
           image = event.images[0].url;
@@ -132,6 +114,8 @@ const SearchResultPage = () => {
           ? event.name.en || event.name.fi || event.name.sv
           : "No name";
 
+        console.log(event?.location["@id"]);
+
         return (
           <Card
             eventImage={image}
@@ -140,7 +124,7 @@ const SearchResultPage = () => {
             name={
               event.name.en || event.name.fi || event.name.sv || event.name.ru
             }
-            locationCall={event.location["@id"]}
+            locationCall={event.location ? event.location["@id"] : ""}
             startDate={new Date(event?.start_time).toLocaleDateString()}
             startTime={new Date(event?.start_time).toLocaleTimeString()}
             endDate={
@@ -161,9 +145,6 @@ const SearchResultPage = () => {
           >
             <div>
               <ul>{singleEventTags}</ul>
-              <div onClick={(e) => handleLike(e)}>
-                <BsHeartFill data-id={event.id} />
-              </div>
             </div>
           </Card>
         );
