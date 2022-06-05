06f5090ce7d0e2b734ea6fd1ac2143b184009f5d
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-06-05 15:00:10 +0300

    implemented functionality to increase interestcount and likecount

--- assets/components/Card/Card.jsx
+++ assets/components/Card/Card.jsx
@@ -47,9 +47,9 @@ function Card(props) {
       />
 
       <div>
-        <Link onClick={props.addInterest} className="name">
+        <h1 onClick={props.addInterest} className="name">
           {props.name}
-        </Link>
+        </h1>
         <p className="dateTime">
           {props.startDate} {props.startTime} - {props.endDate} {props.endTime}
         </p>
--- assets/components/SearchResultPage/SearchResultPage.js
+++ assets/components/SearchResultPage/SearchResultPage.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BsHeartFill } from "react-icons/bs";
+
 import Card from "../Card/Card";
 import Loading from "../Loading/Loading";
 import "./SearchResultPage.scss";
@@ -10,10 +12,12 @@ const SearchResultPage = () => {
   const [events, setEvents] = useState(searchResult.data);
   const [meta, setMeta] = useState(searchResult.meta);
   const [tags, setTags] = useState([]);
+  const [likes, setLikes] = useState([]);
+
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const getTags = (array) => {
+  const getTagsAndLikes = (array) => {
     const getTags = array.map((tag, i) => {
       return tag.keywords.map((singleEvent) => {
         if (!singleEvent["@id"]) {
@@ -30,12 +34,20 @@ const SearchResultPage = () => {
     allTags
       .then(axios.spread((...res) => setTags(res)))
       .catch((err) => console.log("loading tags returned: ", err))
-      .finally(() => setLoading(false));
+      .finally(() =>
+        axios
+          .get("http://127.0.0.1:8000/spa/getlikes")
+          .then((res) => setLikes(res.data))
+          .catch((err) =>
+            console.log("getting event likes returned an error: ", err)
+          )
+          .finally(() => setLoading(false))
+      );
   };
 
   useEffect(() => {
     setLoading(true);
-    getTags(events);
+    getTagsAndLikes(events);
   }, []);
 
   const changePage = (fetch) => {
@@ -45,7 +57,7 @@ const SearchResultPage = () => {
       .then((res) => {
         setEvents(res.data.data);
         setMeta(res.data.meta);
-        getTags(res.data.data);
+        getTagsAndLikes(res.data.data);
       })
       .catch((err) => console.log("loading new events returned: ", err))
       .finally(() => setLoading(false));
@@ -64,6 +76,39 @@ const SearchResultPage = () => {
         window.location.reload();
       })
       .catch((err) => console.log("loading events by tag returned: ", err));
+  };
+
+  const handleLike = (id, endTime, addLike, addInterest) => {
+    const eventHasLikes = likes.filter((like) => like.eventId.includes(id));
+    if (eventHasLikes.length > 0) {
+      console.log("id: ", eventHasLikes[0].id);
+      axios
+        .put(`http://127.0.0.1:8000/spa/updatelike/${eventHasLikes[0].id}`, {
+          eventId: id,
+          endDate: endTime,
+          likeCount: addLike,
+          interestCount: addInterest,
+        })
+        .then((res) => console.log("res: ", res))
+        .catch((err) =>
+          console.log("updating likes or interestCount returned error: ", err)
+        );
+    } else {
+      let postForm = new FormData();
+      postForm.append("likeCount", addLike);
+      postForm.append("interestCount", addInterest);
+      postForm.append("eventId", id);
+      postForm.append("endDate", endTime);
+      axios
+        .post("http://127.0.0.1:8000/spa/addlikes", postForm)
+        .then((res) => console.log("form posted", res))
+        .catch((err) => console.log("error occurred: ", err))
+        .finally(() => {
+          axios
+            .get("http://127.0.0.1:8000/spa/getlikes")
+            .then((res) => setLikes(res.data));
+        });
+    }
   };
 
   if (loading) {
@@ -132,8 +177,17 @@ const SearchResultPage = () => {
               event.short_description?.sv ||
               event.short_description?.ru
             }
+            addInterest={() => {
+              handleLike(event.id, event.end_time, 0, 1);
+              navigate(`/events/${event.id}`);
+            }}
           >
             {<ul>{singleEventTags}</ul>}
+            <div>
+              <BsHeartFill
+                onClick={() => handleLike(event.id, event.end_time, 1, 0)}
+              />
+            </div>
           </Card>
         );
       })}
